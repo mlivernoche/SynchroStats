862e49ce72cc4569019668d6de5bfde9aea1e25e
Matthew Livernoche <mattalivernoche@gmail.com>
2024-06-29 21:30:17 -0700

    Added overload for calculating standard deviation, also some guards for validating arguments

--- src/Data/Math/StandardDeviation.cs
+++ src/Data/Math/StandardDeviation.cs
@@ -1,22 +1,50 @@
-﻿namespace SynchroStats.Data.Math;
+﻿using CommunityToolkit.Diagnostics;
+using SynchroStats.Features.Assessment;
+
+namespace SynchroStats.Data.Math;
 
 public static class StandardDeviation
 {
+    public static double CalculateStandardDeviation<TCardGroup, TCardGroupName, TAssessment>(this HandAssessmentAnalyzer<TCardGroup, TCardGroupName, TAssessment> analyzer, Func<TAssessment, double> selector)
+        where TCardGroup : ICardGroup<TCardGroupName>
+        where TCardGroupName : notnull, IEquatable<TCardGroupName>, IComparable<TCardGroupName>
+        where TAssessment : IHandAssessment<TCardGroupName>
+    {
+        var mean = analyzer.Assessments.Average(selector);
+
+        Guard.IsGreaterThanOrEqualTo(mean, 0.0);
+
+        var total = 0.0;
+        var count = 0;
+
+        foreach (var data in analyzer.Assessments)
+        {
+            total += System.Math.Pow(selector(data) - mean, 2.0);
+            count++;
+        }
+
+        Guard.IsGreaterThan(count, 0);
+        Guard.IsGreaterThanOrEqualTo(total, 0.0);
+
+        return System.Math.Sqrt(total / count);
+    }
+
     public static double CalculateStandardDeviation<T>(this IEnumerable<T> assessments, Func<T, double> selector)
     {
         var count = assessments.Count();
-        if (count == 0)
-        {
-            return 0.0;
-        }
+        Guard.IsGreaterThan(count, 0);
 
         var mean = assessments.Average(selector);
+        Guard.IsGreaterThanOrEqualTo(mean, 0.0);
+
         var total = 0.0;
 
         foreach (var data in assessments)
         {
             total += System.Math.Pow(selector(data) - mean, 2.0);
         }
+
+        Guard.IsGreaterThanOrEqualTo(total, 0.0);
 
         return System.Math.Sqrt(total / count);
     }
